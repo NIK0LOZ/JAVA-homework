edd95b1b5010a2f1dbbc016c4875f97ba82a257a
Nika <nikolozmelikidze85@gmail.com>
2026-01-31 13:26:12 +0400

    add homework

---
+++ src/Homework/FirstHomework.java
@@ -0,0 +1,51 @@
+package Homework;
+
+public class FirstHomework {
+
+    public static void main(String[] args) {
+        /*
+        int a = 10;
+        int b = 5;
+        int c = (a / b);
+        int d = (a * b);
+        int e = (a - b);
+        int f = (a + b);
+
+
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        */
+
+
+        /*
+
+        int a = 10;
+        int b = 20;
+
+        boolean სწორია = a < 15 && b > 15;
+        boolean არასწორია = a > 11 && b > 15;
+
+        System.out.println("სწორია");
+        System.out.println("არასწორია");
+        */
+
+        /*
+
+        int a = 10;
+        int b= 20;
+        boolean სწორია = a>5 || b<18;
+        boolean არასწორია = a<5 || b<18;
+        System.out.println("სწორია");
+        System.out.println("არასწორია");
+
+        */
+        int birthyear = 1985;
+        int currentYear = 2026;
+        int age = currentYear-birthyear;
+
+        System.out.println("ჩემი დაბადების წელია " + birthyear+ " ჩემი ასაკია " + age + " ");
+    }
+
+}
