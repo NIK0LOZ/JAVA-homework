28c5759e7700f3cdae20e13ec3979b0914acbddc
Nika <nikolozmelikidze85@gmail.com>
2026-02-19 21:25:37 +0400

    add shualeduri

---
+++ src/shualeduri.java
@@ -0,0 +1,30 @@
+public class shualeduri {
+
+
+    public static boolean equal(int a, int b) {
+        return a == b;
+    }
+
+    public static void main(String[] args) {
+
+
+        /*for (int i = 0; i < 10; i++)
+        {
+            System.out.println("Hello world");
+        }*/
+
+
+       /* System.out.println( equal(5, 3));
+        System.out.println(equal(2, 2));
+        System.out.println(equal(14, 41));*/
+
+        /*String [] cityArray = {"New York", "San Francisco", "Dallas", "Los Angeles", "Seattle"};
+        for (int i = 0; i < cityArray.length; i++) {
+            System.out.println(cityArray[i]);
+        }*/
+
+
+
+
+    }
+}
